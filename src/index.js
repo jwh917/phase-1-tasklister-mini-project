5f8fc77b4e15342fedee5937c553ec031a76339a
Julien Hnery <jwh_917@yahoo.com>
2022-04-27 09:14:51 -0400

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,59 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+
+  const form = document.getElementById("create-task-form")
+
+  form.addEventListener("submit", function (event){
+    event.preventDefault()
+    renderList()
+
+  })
+
+
+})
+
+
+function renderList(){
+  let newTaskInput = document.getElementById("new-task-description").value
+  // console.log(newTaskInput)
+
+  let myTodos = document.getElementById("tasks")
+  let ulNewTask = document.createElement("li")
+
+  ulNewTask.innerHTML = newTaskInput
+
+  //creates delete buttons
+  ulNewTask.innerHTML += ' <button onclick="this.parentNode.remove()">X</button>'
+
+
+  // let xBtn = document.createElement("button")
+  // xBtn.innerHTML = "X"
+  // xBtn.setAttribute("id", "deleteButton")
+
+  myTodos.appendChild(ulNewTask)
+
+  // myTodos.appendChild(xBtn)
+
+  document.getElementById("new-task-description").value = ""
+
+  // deleteTask()
+
+}
+
+// function deleteTask(){
+
+
+//   // let deleteBtn = document.getElementById("deleteButton")
+
+//   // deleteBtn.addEventListener("click", function(){
+//   //   console.log("will delete soon")
+
+//   // })
+
+
+
+
+// }
+
+
+
