dc1d44077cf1f372a719b2e60a80de32c3218b48
Julien Hnery <jwh_917@yahoo.com>
2022-04-27 09:34:29 -0400

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -20,7 +20,11 @@ function renderList(){
   let myTodos = document.getElementById("tasks")
   let ulNewTask = document.createElement("li")
 
-  ulNewTask.innerHTML = newTaskInput
+  // let date = document.createElement("p")
+
+  ulNewTask.innerHTML = `${newTaskInput}, ${dateStamp()}`
+
+  // date.innerHTML = 
 
   //creates delete buttons
   ulNewTask.innerHTML += ' <button onclick="this.parentNode.remove()">X</button>'
@@ -32,28 +36,34 @@ function renderList(){
 
   myTodos.appendChild(ulNewTask)
 
+  // myTodos.appendChild(date)
+
   // myTodos.appendChild(xBtn)
 
   document.getElementById("new-task-description").value = ""
 
   // deleteTask()
+  
 
 }
 
 // function deleteTask(){
-
-
 //   // let deleteBtn = document.getElementById("deleteButton")
-
 //   // deleteBtn.addEventListener("click", function(){
 //   //   console.log("will delete soon")
-
 //   // })
-
-
-
-
 // }
 
+function dateStamp(){
+  const currentDate = new Date();
 
+  const currentDayOfMonth = currentDate.getDate()
+  const currentMonth = currentDate.getMonth()
+  const currentYear = currentDate.getFullYear()
+
+  const dateString = (currentMonth + 1) + "-" +  currentDayOfMonth + "-" + currentYear
+
+
+  return `Date: ${dateString}`
+  }
 
